c2ea5e8133da2b8fc074180c8c81649b45272406
Joseph Vero <joev714@Gmail.com>
2017-08-20 14:45:14 -0400

    beginning of my project

---
+++ config/config.go
@@ -0,0 +1,25 @@
+package config
+
+import (
+	"fmt"
+	"io/ioutil"
+)
+
+// Public Variable
+var (
+	Token string
+)
+
+// ReadConfig Returns the token to use in the API
+func ReadConfig() (string, error) {
+	fmt.Println("Reading from API key file")
+
+	file, err := ioutil.ReadFile("./apikey.txt")
+	if err != nil {
+		fmt.Println(err.Error())
+		return "", err
+	}
+
+	Token = string(file)
+	return Token, nil
+}
